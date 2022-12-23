c066045bed2b08401fe1fbb2a65704c564119dc0
tqk2811 <tqk2811@gmail.com>
2022-12-23 17:14:19 +0700

    build script: auto copy to localNuget

--- Build.ps1
+++ Build.ps1
@@ -10,10 +10,12 @@ devenv .\TqkLibrary.Scrcpy.sln /Rebuild 'Release|x86' /Project TqkLibrary.Scrcpy
 dotnet build --no-incremental .\TqkLibrary.Scrcpy\TqkLibrary.Scrcpy.csproj -c Release
 nuget pack .\TqkLibrary.Scrcpy\TqkLibrary.Scrcpy.nuspec -Symbols -OutputDirectory .\TqkLibrary.Scrcpy\bin\Release
 
-
 dotnet build --no-incremental .\TqkLibrary.Scrcpy.Wpf\TqkLibrary.Scrcpy.Wpf.csproj -c Release
 nuget pack .\TqkLibrary.Scrcpy.Wpf\TqkLibrary.Scrcpy.Wpf.nuspec -Symbols -OutputDirectory .\TqkLibrary.Scrcpy.Wpf\bin\Release
 
-
-
-pause
+$localNuget = $env:localNuget
+if(![string]::IsNullOrWhiteSpace($localNuget))
+{
+    Copy-Item .\TqkLibrary.Scrcpy\bin\Release\*.nupkg -Destination $localNuget -Force
+    Copy-Item .\TqkLibrary.Scrcpy.Wpf\bin\Release\*.nupkg -Destination $localNuget -Force
+}
