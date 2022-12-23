1aeafb2ad5786303c14d19f54b452435172a7db5
tqk2811 <tqk2811@gmail.com>
2022-12-23 12:40:07 +0700

    add exceptions

---
+++ TqkLibrary.Scrcpy/Exceptions/InvalidRangeException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TqkLibrary.Scrcpy.Exceptions
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class InvalidRangeException : ScrcpyException
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public InvalidRangeException() { }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="message"></param>
+        public InvalidRangeException(string message) : base(message) { }
+    }
+}
---
+++ TqkLibrary.Scrcpy/Exceptions/ScrcpyException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TqkLibrary.Scrcpy.Exceptions
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ScrcpyException : Exception
+    {
+        internal ScrcpyException() { }
+        internal ScrcpyException(string message) : base(message) { }
+    }
+}
