81f7ed774952e190d24186415f836266c007f5b3
tqk2811 <tqk2811@gmail.com>
2023-12-02 17:26:21 +0700

    add OptionNameAttribute

---
+++ TqkLibrary.Scrcpy/Attributes/OptionNameAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TqkLibrary.Scrcpy.Attributes
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class OptionNameAttribute : Attribute
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public OptionNameAttribute(string name)
+        {
+            this.Name = name;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Name { get; }
+    }
+}
